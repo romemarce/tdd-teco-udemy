2aa1deec09746f75f69dc60633367a311f34559e
Marcelo Romero <mrromero@teco.com.ar>
2025-03-17 10:23:53 -0300

    Mocks: modules

--- src/__tests__/example_moks.test.js
+++ src/__tests__/example_moks.test.js
@@ -1,18 +1,49 @@
-const { toHaveFormValues } = require("@testing-library/jest-dom/matchers");
+// const { toHaveFormValues } = require("@testing-library/jest-dom/matchers");
+import { storage } from "../lib/storage";
+import { getUsername, saveUsername } from "../user";
+jest.mock('../lib/storage');
 
-test('first example', () => {
-    const myMock = jest.fn()
+test("first example", () => {
+  const myMock = jest
+    .fn()
     .mockReturnValueOnce(true)
-    .mockReturnValueOnce('Hi')
-    .mockReturnValueOnce(5)
+    .mockReturnValueOnce("Hi")
+    .mockReturnValueOnce(5);
 
-    const result1 = myMock();
-    const result2 = myMock();
-    const result3 = myMock();
+  const result1 = myMock();
+  const result2 = myMock();
+  const result3 = myMock();
 
-    expect(myMock).toHaveBeenCalledTimes(3)
+  expect(myMock).toHaveBeenCalledTimes(3);
 
-    expect(result1).toBe(true)
-    expect(result2).toBe('Hi')
-    expect(result3).toBe(5)
-})
+  expect(result1).toBe(true);
+  expect(result2).toBe("Hi");
+  expect(result3).toBe(5);
+});
+
+test("second example", () => {
+  const username = "john doe";
+
+  saveUsername(username);
+
+  expect(storage.save).toHaveBeenCalledTimes(1);
+
+  expect(storage.save).toHaveBeenCalledWith({
+    key: "username",
+    value: username,
+  });
+});
+
+test("third example", () => {
+  const username = "john doe";
+
+  storage.get.mockReturnValueOnce(username);
+
+  const result = getUsername();
+
+  expect(result).toBe(username);
+
+  expect(storage.get).toHaveBeenCalledTimes(1);
+
+  expect(storage.get).toHaveBeenCalledWith({ key: "username" });
+});
