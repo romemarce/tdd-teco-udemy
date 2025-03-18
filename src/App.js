b4800868271360f9e904eee427ef8c61483962a7
Marcelo Romero <mrromero@teco.com.ar>
2025-03-18 08:50:10 -0300

    Lindo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { HelloWorld } from "./components/HelloWorld/HelloWorld";
+import { Counter } from "./components/Counter/Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {" "}
+          Edit <code>src/App.js</code> and save to reload.{" "}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="container">
+          <HelloWorld />
+          <Counter />
+        </section>
       </header>
     </div>
   );
